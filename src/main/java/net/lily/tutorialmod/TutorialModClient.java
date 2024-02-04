7ba5738334d773fdaee4cb15ba234932717a27bb
LilyBottom <lilybottom69@gmial.com>
2024-02-04 13:47:35 -0800

    ^^

---
+++ src/main/java/net/lily/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.lily.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
